5a7a9aae70b7b1a2af3e786607df0a65172c8123
Matt Wheeler <wheelermt@proton.me>
2025-10-15 06:56:58 +0100

    Implement basic Pascal triangle construction

--- src/chapter2/main.cpp
+++ src/chapter2/main.cpp
@@ -3,7 +3,30 @@
 //
 
 #include <iostream>
+#include <vector>
+
+std::vector<int> get_next_row(const std::vector<int>& last_row) {
+    std::vector next_row{1};
+    if (last_row.empty()) {
+        return next_row;
+    }
+    for (size_t idx = 0; idx + 1 < last_row.size(); ++idx) {
+        next_row.emplace_back(last_row[idx] + last_row[idx + 1]);
+    }
+    next_row.emplace_back(1);
+    return next_row;
+}
+
+auto generate_triangle(const int rows) {
+    std::vector<int> data;
+    std::vector<std::vector<int>> triangle;
+    for (int row = 0; row < rows; ++row) {
+        data = get_next_row(data);
+        triangle.push_back(data);
+    }
+    return triangle;
+}
 
 int main() {
-    std::cout << "Hello, world!\n";
+    generate_triangle(3);
 }
