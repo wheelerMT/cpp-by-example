91cec6e5b2c1da108c65af886ce2a7d37aee005c
Matt Wheeler <wheelermt@proton.me>
2025-10-17 15:19:39 +0100

    Add error handling in the form of asserts

--- src/chapter2/main.cpp
+++ src/chapter2/main.cpp
@@ -3,9 +3,37 @@
 //
 
 #include <algorithm>
+#include <cassert>
 #include <iostream>
 #include <iterator>
+#include <numeric>
+#include <ranges>
 #include <vector>
+
+bool is_palindrome(const std::vector<int>& v) {
+    auto forward = v | std::views::take(v.size() / 2);
+    auto backward = v | std::views::reverse | std::views::take(v.size() / 2);
+
+    return std::ranges::equal(forward, backward);
+}
+
+void check_properties(const std::vector<std::vector<int>>& triangle) {
+    size_t row_number = 1;
+    int expected_total = 1;
+    for (const auto& row : triangle) {
+        assert(row.front() == 1);
+        assert(row.back() == 1);
+        assert(row.size() == row_number);
+        assert(std::accumulate(row.begin(), row.end(), 0) == expected_total);
+        assert(std::ranges::all_of(row, [](auto x) {
+            return x > 0;
+        }));
+        assert(is_palindrome(row));
+
+        expected_total *= 2;
+        row_number++;
+    }
+}
 
 std::vector<int> get_next_row(const std::vector<int>& last_row) {
     std::vector next_row{1};
@@ -20,7 +48,7 @@ std::vector<int> get_next_row(const std::vector<int>& last_row) {
 }
 
 auto generate_triangle(const int rows) {
-    std::vector<std::vector<int>> triangle{1};
+    std::vector<std::vector<int>> triangle{{1}};
     for (int row = 0; row < rows; ++row) {
         triangle.push_back(get_next_row(triangle.back()));
     }
@@ -37,15 +65,19 @@ std::ostream& operator<<(std::ostream& s, const std::vector<std::vector<T>>& tri
 }
 
 void show_vectors(std::ostream& s, const std::vector<std::vector<int>>& v) {
-    const size_t final_row_size = v.back().size();
-    std::string spaces(final_row_size * 3, ' ');
+
+    std::string spaces(v.back().size() * 3, ' ');
     for (const auto& row : v) {
         s << spaces;
-        if (spaces.size() > 3) {
-            spaces.resize(spaces.size() - 3);
+        if (!spaces.empty()) {
+            spaces.resize(spaces.size() - 1);
         }
-        for (const auto& data : row) {
-            s << std::format("{: ^{}}", data, 6);
+        auto odds = row | std::views::transform([](auto x) {
+                        return x % 2 == 0 ? '*' : ' ';
+                    });
+
+        for (const auto& data : odds) {
+            s << data << ' ';
         }
         s << '\n';
     }
@@ -53,5 +85,6 @@ void show_vectors(std::ostream& s, const std::vector<std::vector<int>>& v) {
 
 int main() {
     const auto triangle = generate_triangle(16);
+    check_properties(triangle);
     show_vectors(std::cout, triangle);
 }
