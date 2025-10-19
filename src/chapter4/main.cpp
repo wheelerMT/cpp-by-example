533d428513e77b3ab6cec1f7f79d1bf9e1832e7b
Matt Wheeler <wheelermt@proton.me>
2025-10-19 11:19:17 +0100

    Update code to use more readable types

--- src/chapter4/main.cpp
+++ src/chapter4/main.cpp
@@ -2,13 +2,17 @@
 #include <ostream>
 
 void duration_to_end_of_year() {
-    const std::chrono::time_point now = std::chrono::system_clock::now();
-    constexpr auto year = 2025;
-    constexpr auto new_years_eve = std::chrono::year_month_day(
-        std::chrono::year(year), std::chrono::month(12), std::chrono::day(31));
-    constexpr auto event = std::chrono::sys_days(new_years_eve);
-    std::chrono::duration duration = event - now;
-    std::println("Duration to event: {}", duration);
+    using namespace std::chrono;
+    const time_point now = system_clock::now();
+    const auto ymd = year_month_day{floor<days>(now)};
+
+    const auto this_year = ymd.year();
+    const auto new_years_eve = this_year / December / 31;
+
+    const auto event = sys_days(new_years_eve);
+    const duration dur = event - now;
+
+    std::println("Duration to event: {}", duration_cast<days>(dur));
 }
 
 int main() {
