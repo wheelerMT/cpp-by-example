af10de2d7827a716a3c5ced67200fdce87c0b00f
Matt Wheeler <wheelermt@proton.me>
2025-10-20 06:25:42 +0100

    Add pay_day function

--- src/chapter4/main.cpp
+++ src/chapter4/main.cpp
@@ -15,6 +15,20 @@ void duration_to_end_of_year() {
     std::println("Duration to event: {}", duration_cast<days>(dur));
 }
 
+void pay_day() {
+    using namespace std::chrono;
+
+    const time_point now = system_clock::now();
+    const auto ymd = year_month_day{floor<days>(now)};
+    const auto pay_day = ymd.year() / ymd.month() / Friday[last];
+
+    const auto event = sys_days(pay_day);
+    const duration dur = event - now;
+
+    std::println("You have {} until pay day", duration_cast<days>(dur));
+}
+
 int main() {
     duration_to_end_of_year();
+    pay_day();
 }
