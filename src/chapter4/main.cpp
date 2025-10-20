59017204c54d1cf199c4552b985b204dbceec4e4
Matt Wheeler <wheelermt@proton.me>
2025-10-20 06:45:29 +0100

    Add function to return a given duration for testing purposes

--- src/chapter4/main.cpp
+++ src/chapter4/main.cpp
@@ -1,3 +1,4 @@
+#include <__thread/this_thread.h>
 #include <chrono>
 #include <ostream>
 
@@ -28,7 +29,36 @@ void pay_day() {
     std::println("You have {} until pay day", duration_cast<days>(dur));
 }
 
+constexpr std::chrono::system_clock::duration
+countdown(const std::chrono::system_clock::time_point start) {
+    using namespace std::chrono;
+
+    const auto days_only = floor<days>(start);
+    const auto ymd = year_month_day{days_only};
+    const auto this_year = ymd.year();
+    const auto new_years_eve = this_year / December / last;
+
+    const auto event = sys_days(new_years_eve);
+    return event - start;
+}
+
+void check_properties() {
+    using namespace std::chrono;
+    constexpr auto new_years_eve = 2025y / December / last;
+    constexpr auto one_day_away = sys_days{new_years_eve} - 24h;
+    constexpr auto result = countdown(one_day_away);
+    static_assert(duration_cast<days>(result) == days{1});
+}
+
 int main() {
     duration_to_end_of_year();
     pay_day();
+    check_properties();
+
+    using namespace std::chrono;
+    for (int i = 0; i < 5; ++i) {
+        std::this_thread::sleep_for(1000ms);
+        auto dur = countdown(system_clock::now());
+        std::println("{} until event", duration_cast<seconds>(dur));
+    }
 }
