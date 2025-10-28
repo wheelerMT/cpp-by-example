1068506383a4144a7278d5a3cf3ab7e76d1d449a
Matt Wheeler <wheelermt@proton.me>
2025-10-28 21:19:14 +0000

    Implement `shuffle_deck` function

--- src/chapter5/main.cpp
+++ src/chapter5/main.cpp
@@ -1,7 +1,19 @@
 #include "playing_cards.h"
+#include <random>
+
+#include <algorithm>
 using namespace cards;
 
+void shuffle_deck(Deck& deck) {
+    std::random_device rd;
+    std::mt19937 gen{rd()};
+    std::ranges::shuffle(deck, gen);
+}
+
 int main() {
-    const Card card{FaceValue(11), Suit::Clubs};
-    std::cout << card << '\n';
+    auto deck = create_deck();
+    shuffle_deck(deck);
+    for (auto& card : deck) {
+        std::cout << card << '\n';
+    }
 }
