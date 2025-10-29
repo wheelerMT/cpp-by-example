ec44cbafdd5b4a778f22aeaaf0c2dd3f33ed74d0
Matt Wheeler <wheelermt@proton.me>
2025-10-29 06:29:40 +0000

    Add higher_lower game functionality

--- src/chapter5/main.cpp
+++ src/chapter5/main.cpp
@@ -14,10 +14,25 @@ bool is_guess_correct(const char guess, const Card& current, const Card& next) {
     return (guess == 'h' && next > current) | (guess == 'l' && next < current);
 }
 
-int main() {
+void higher_lower() {
     auto deck = create_deck();
     shuffle_deck(deck);
-    for (auto& card : deck) {
-        std::cout << card << '\n';
+
+    size_t index = 0;
+    while (index + 1 < deck.size()) {
+        std::cout << deck[index] << ": Next card (h)igher or (l)ower?\n";
+        char c;
+        std::cin >> c;
+        if (const bool ok = is_guess_correct(c, deck[index], deck[index + 1]); !ok) {
+            std::cout << "Next card was " << deck[index + 1] << '\n';
+            break;
+        }
+        ++index;
     }
+
+    std::cout << "You got " << index << " correct\n";
+}
+
+int main() {
+    higher_lower();
 }
