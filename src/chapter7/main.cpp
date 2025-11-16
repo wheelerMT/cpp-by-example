3c03bf4aaba6b3e22d330971b1035df7b772df8d
Matt Wheeler <wheelermt@proton.me>
2025-11-16 21:17:12 +0000

    Add basic warm up code

--- src/chapter7/main.cpp
+++ src/chapter7/main.cpp
@@ -2,7 +2,18 @@
 // Created by Matthew Wheeler on 16/11/2025.
 //
 
-#include <ostream>
+#include <iostream>
+#include <map>
+
+void warm_up() {
+    std::map<std::string, std::string> dictionary;
+    dictionary["assume"] = "take for granted, take to be the case";
+    std::string new_value = dictionary["fictional"];
+    for (const auto& [key, value] : dictionary) {
+        std::println("{} : {}", key, value);
+    }
+}
+
 int main() {
-    std::println("Hello, world!");
+    warm_up();
 }
